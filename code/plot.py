4cd68c23723517ae6f653c0e032ebb89c6a2fb55
Ken Wang <kw1166@della-gpu.princeton.edu>
2023-02-01 19:25:24 -0500

    fix for standard error

--- code/plot.py
+++ code/plot.py
@@ -3,6 +3,7 @@ import glob
 import os
 import matplotlib.pyplot as plt
 import pandas as pd
+import numpy as np
 
 from aggregate_results import get_results
 
@@ -14,17 +15,28 @@ def plot(args):
     else:
         df = get_results()
 
+
     # Plot each query as a separate line
     for query, label in zip(args.q, args.labels):
         sub_df = df.query(query).sort_values(by=args.x)
         x = sub_df[args.x]
         y = sub_df[args.y]
-
         assert not y.isna().all(), "all nans"
 
         if args.yerr is not None:
             yerr = sub_df[args.yerr]
-            plt.fill_between(x, y - yerr, y + yerr, alpha=0.2)
+            n = sub_df["avg_test_nn_rocauc_n"]
+            yem = yerr / np.sqrt(n)
+            # plt.fill_between(x, y - yem, y + yem, alpha=0.2)
+
+            lags = np.arange(-4000,4100,100)
+            yem2 = []
+            for lag in lags:
+                lag_folder = os.path.join("results", sub_df.model.unique()[0], str(lag))
+                csv_1 = os.path.join(lag_folder, "ensemble", "avg_test_topk_rocaauc_df.csv")
+                results = pd.read_csv(csv_1).rocauc
+                yem2.append(results.sem())
+            plt.fill_between(x, y - yem2, y + yem2, alpha=0.4)
 
         if args.label is not None:
             label = sub_df.iloc[0][args.label]
@@ -43,19 +55,6 @@ def plot(args):
 
         plt.plot(x, y, label=label, color=color)
 
-    # breakpoint()
-    lags = glob.glob(os.path.join("results", args.sig, "*"))
-    for lag_folder in lags:
-        lag_name = os.path.basename(lag_folder)
-        csv_1 = os.path.join(
-            lag_folder, "ensemble", "avg_test_topk_rocaauc_df.csv"
-        )
-        csv_2 = csv_1.replace("IFG", "PRE")
-        results = pd.read_csv(csv_1)
-        results2 = pd.read_csv(csv_2)
-        # function
-
-        breakpoint()
 
     plt.grid()
     plt.legend(loc="upper left")
